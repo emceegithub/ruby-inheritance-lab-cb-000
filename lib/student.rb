7dea65a0525b70bb4c9e674ae9a7d5105da684b7
Mark Cruz <markchriscruz@yahoo.com>
2018-03-18 04:38:11 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,11 @@
 require_relative "./user.rb"
 class Student < User
-  attr_accessor :knowledge
   
   def initialize
     @knowledge = []
   end
 
+  def knowledge
+    return @knowledge
+  end
 end
